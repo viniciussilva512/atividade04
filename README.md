05c14f14ee8dd399900b0574508baf1439c9a081
viniciussilva512 <marvosvinicius128@gmail.com>
2023-10-11 11:41:57 -0300

    adicionei atividade04

---
+++ README.md
@@ -0,0 +1,1 @@
+# atividade04
---
+++ atividade04.py
@@ -0,0 +1,5 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+numero = int(input("digite um número"))
+sucessor = (numero + 1)
+antecessor = (numero - 1)
+print(f"O sucessor do número é {sucessor}, e seu antecessor é {antecessor}")
